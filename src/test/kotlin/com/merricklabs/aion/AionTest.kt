57773e3f9a1e510a2e564f3de9512cd63ce2ea68
David Merrick <david@david-merrick.com>
2019-06-20 18:51:11 -0700

    Got copy working with events

--- src/test/kotlin/com/merricklabs/aion/AionTest.kt
+++ src/test/kotlin/com/merricklabs/aion/AionTest.kt
@@ -1,12 +1,13 @@
 package com.merricklabs.aion
 
 import biweekly.Biweekly
+import biweekly.ICalendar
+import biweekly.component.VEvent
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.testng.annotations.Test
 
 
-// For now, just testing out an iCal lib
 class AionTest {
 
     @Test
@@ -21,12 +22,20 @@ class AionTest {
         val bodyString = response.body()!!.string()
         val calendar = Biweekly.parse(bodyString).first()
 
+
         val filtered = calendar.events.asSequence()
                 .filter { !it.summary.value.toLowerCase().contains("microbiome") }
                 .toList()
 
-        // Todo: Create a new calendar with the filtered events and return it
+        val cloned = copyWithEvents(calendar, filtered)
+        val calendarString = Biweekly.write(cloned).go()
+        println(calendarString)
+    }
 
-        println("foo")
+    fun copyWithEvents(original: ICalendar, events: List<VEvent>): ICalendar {
+        val newCalendar = ICalendar(original)
+        newCalendar.setComponent(VEvent::class.java, null)
+        events.forEach { newCalendar.addEvent(it) }
+        return newCalendar
     }
 }
