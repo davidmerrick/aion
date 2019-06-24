89d127096ab566a590fd211faad47455b1851bbe
David Merrick <david@david-merrick.com>
2019-06-23 17:52:54 -0700

    Added @DynamoDBDocument

--- src/main/kotlin/com/merricklabs/aion/handlers/models/FieldFilter.kt
+++ src/main/kotlin/com/merricklabs/aion/handlers/models/FieldFilter.kt
@@ -1,7 +1,9 @@
 package com.merricklabs.aion.handlers.models
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument
 
+@DynamoDBDocument
 data class FieldFilter(
         @DynamoDBAttribute var include: List<String>?,
         @DynamoDBAttribute var exclude: List<String>?
