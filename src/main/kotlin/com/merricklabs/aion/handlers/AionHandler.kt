08b7a09c1152c8ab71098f428b26da9f5525cda8
David Merrick <david@david-merrick.com>
2019-06-27 09:13:25 -0700

    Added trace logging for requests

--- src/main/kotlin/com/merricklabs/aion/handlers/AionHandler.kt
+++ src/main/kotlin/com/merricklabs/aion/handlers/AionHandler.kt
@@ -4,11 +4,16 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.merricklabs.aion.AionModule
 import com.merricklabs.aion.handlers.util.AionLogic
 import com.merricklabs.aion.handlers.util.ResourceHelpers
+import mu.KotlinLogging
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
+import org.koin.core.inject
+
+private val log = KotlinLogging.logger {}
 
 abstract class AionHandler : KoinComponent, RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     protected abstract fun getLogic(): AionLogic
@@ -19,9 +24,11 @@ abstract class AionHandler : KoinComponent, RequestHandler<APIGatewayProxyReques
         }
     }
 
-    override fun handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
+    override fun handleRequest(request: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
+        val mapper by inject<ObjectMapper>()
+        log.trace(mapper.writeValueAsString(request))
         return try {
-            getLogic().handleRequest(input, context)
+            getLogic().handleRequest(request, context)
         } catch (e: Exception) {
             ResourceHelpers.exceptionToWebAppResponse(e)
         }
