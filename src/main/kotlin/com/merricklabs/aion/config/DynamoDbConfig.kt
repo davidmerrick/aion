fe21a5d205aac767d6932a676c77fc5271d462ad
David Merrick <david@david-merrick.com>
2019-07-29 17:06:19 -0700

    Switched config to use localstack for testing

--- src/main/kotlin/com/merricklabs/aion/config/DynamoDbConfig.kt
+++ src/main/kotlin/com/merricklabs/aion/config/DynamoDbConfig.kt
@@ -1,8 +1,8 @@
 package com.merricklabs.aion.config
 
 class DynamoDbConfig {
-    val endpoint = System.getenv("DYNAMODB_ENDPOINT") ?: "http://localhost:8000"
-    val region = System.getenv("DYNAMODB_REGION") ?: "us-east-1"
+    val endpoint = System.getenv("DYNAMODB_ENDPOINT") ?: "http://localhost:4570"
+    val region = System.getenv("DYNAMODB_REGION") ?: "us-west-2"
     val calendarTableName= System.getenv("CALENDAR_TABLE_NAME") ?: "aion-calendar"
     val filterTableName= System.getenv("FILTER_TABLE_NAME") ?: "aion-filter"
 }
